b898e43cacadfb3f9d3734505c8913c26f75b2ae
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-28 17:30:25 -0600

    Add location image to place detail screen and display location address in places list

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -10,6 +10,12 @@ class PlaceDetailScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location!.latitude;
+    final lng = place.location!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDR3lXInhuRIgFbBDas8ftWx3rOnosHD2w';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +24,46 @@ class PlaceDetailScreen extends StatelessWidget {
       ),
       body: Stack(
         children: [
-            Image.file(
-              place.image,
-              fit: BoxFit.cover,
-              width: double.infinity,
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location!.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              ],
             ),
+          )
         ],
       ),
     );
